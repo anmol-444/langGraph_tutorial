641976b8677f7cd45ab22f2c4e6ef6b3ce83ef31
anmol-444 <anmol2708pandey@gmail.com>
2026-08-29 11:50:44 +0530

    Integrated SQLite

---
+++ langgraph_chatbot/langgraph_database_backend.py
@@ -0,0 +1,45 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import sqlite3
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="conversational"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state["messages"]
+    response = model.invoke(messages)
+
+    return {"messages": [response]}
+
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+
+graph = StateGraph(ChatState)
+
+graph.add_node('chat_node', chat_node)
+
+graph.add_edge(START, 'chat_node')
+graph.add_edge('chat_node', END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_thread = set()
+    for checkpoint in checkpointer.list(None):
+        all_thread.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_thread)
